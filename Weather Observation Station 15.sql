4d5f29785cfb97a51af1449747b38dae57164dfa
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-01-04 22:06:18 +0530

    Create Weather Observation Station 15.sql

---
+++ Weather Observation Station 15.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+*/
+
+order by lat_n and then arrange in desc order and select the top element
+
+select round(LONG_W,4)  from STATION where LAT_N < 137.2345 order by LAT_N desc limit 1
