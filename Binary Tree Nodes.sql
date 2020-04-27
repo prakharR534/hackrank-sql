e6b4b3aac7b6ffcd149fc55635f15db9085c9816
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-27 22:35:07 +0530

    Update Binary Tree Nodes.sql

--- Binary Tree Nodes.sql
+++ Binary Tree Nodes.sql
@@ -5,5 +5,6 @@ select BT.N,
 case  when BT.P is NULL then 'Root'
       when exists(select B.P from BST B where B.P = BT.N) then 'Inner'
       else 'Leaf'
+      
 end
 from BST as BT order by BT.N
